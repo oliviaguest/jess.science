462d51bfa462577e3eb01fc3f0dd96dcfe366faf
Olivia Guest <guest.olivia@gmail.com>
2023-10-01 15:34:19 +0200

    alt text

--- js/randomise.js
+++ js/randomise.js
@@ -6,12 +6,12 @@ $(function() {
     console.log(fileNames);
     console.log(fileNames);
     fileNames.sort(function() {return 0.5 - Math.random()});
-    $( "#top-bird" ).attr("src", fileNames[0].innerText);
-    $( "#bottom-bird" ).attr("src", fileNames[1].innerText);
+    $("#top-bird").attr("src", fileNames[0].innerText);
+    $("#top-bird").attr("alt", fileNames[0].innerText.split('/').pop().split('.')[0]);
+    $("#bottom-bird").attr("src", fileNames[1].innerText);
+    $("#bottom-bird").attr("alt", fileNames[1].innerText.split('/').pop().split('.')[0]);
   };
-
 fileNames = $(".image-url");
-console.log(fileNames[0].innerText);
 placeImages();
 });
 
